95a4d164fa3271eb55c8f2f2b9cea05c59327b90
Dean Ganczarski <>
2019-10-18 15:29:23 -0400

    Added images for Generate Aspiration

--- src/App.js
+++ src/App.js
@@ -118,60 +118,60 @@ export class App extends React.Component {
     
     switch(randomValue) {
       // Requires Cats & Dogs
-      case 1: this.setState({label: "Friend of the Animals", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 2: this.setState({label: "Bodybuilder", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 3: this.setState({label: "Painter Extraordinare", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 4: this.setState({label: "Musical Genius", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 5: this.setState({label: "Bestselling Author", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 1: this.setState({label: "Friend of the Animals", imgSrc: require('./images/aspirations/1_friend_of_the_animals.png')}); break;
+      case 2: this.setState({label: "Bodybuilder", imgSrc: require('./images/aspirations/2_bodybuilder.png')}); break;
+      case 3: this.setState({label: "Painter Extraordinare", imgSrc: require('./images/aspirations/3_painter_extraordinare.png')}); break;
+      case 4: this.setState({label: "Musical Genius", imgSrc: require('./images/aspirations/4_musical_genius.png')}); break;
+      case 5: this.setState({label: "Bestselling Author", imgSrc: require('./images/aspirations/5_bestselling_author.png')}); break;
       // Requires Get Famous
-      case 6: this.setState({label: "Master Actor/Actress", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 7: this.setState({label: "Public Enemy", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 8: this.setState({label: "Chief of Mischief", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 9: this.setState({label: "Successful Lineage", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 10: this.setState({label: "Big Happy Family", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 6: this.setState({label: "Master Actor/Actress", imgSrc: require('./images/aspirations/6_master_actor.png')}); break;
+      case 7: this.setState({label: "Public Enemy", imgSrc: require('./images/aspirations/7_public_enemy.png')}); break;
+      case 8: this.setState({label: "Chief of Mischief", imgSrc: require('./images/aspirations/8_chief_of_mischief.png')}); break;
+      case 9: this.setState({label: "Successful Lineage", imgSrc: require('./images/aspirations/9_successful_lineage.png')}); break;
+      case 10: this.setState({label: "Big Happy Family", imgSrc: require('./images/aspirations/10_big_happy_family.png')}); break;
       // Requires Vampires
-      case 11: this.setState({label: "Vampire Family", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 11: this.setState({label: "Vampire Family", imgSrc: require('./images/aspirations/11_vampire_family.png')}); break;
       // Requires Parenthood
-      case 12: this.setState({label: "Super Parent", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 13: this.setState({label: "Master Chef", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 14: this.setState({label: "Master Mixologist", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 15: this.setState({label: "Fabulously Wealthy", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 16: this.setState({label: "Mansion Baron", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 17: this.setState({label: "Renaissance Sim", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 18: this.setState({label: "Nerd Brain", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 19: this.setState({label: "Computer Whiz", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 12: this.setState({label: "Super Parent", imgSrc: require('./images/aspirations/12_super_parent.png')}); break;
+      case 13: this.setState({label: "Master Chef", imgSrc: require('./images/aspirations/13_master_chef.png')}); break;
+      case 14: this.setState({label: "Master Mixologist", imgSrc: require('./images/aspirations/14_master_mixologist.png')}); break;
+      case 15: this.setState({label: "Fabulously Wealthy", imgSrc: require('./images/aspirations/15_fabulously_wealthy.png')}); break;
+      case 16: this.setState({label: "Mansion Baron", imgSrc: require('./images/aspirations/16_mansion_baron.png')}); break;
+      case 17: this.setState({label: "Renaissance Sim", imgSrc: require('./images/aspirations/17_renaissance_sim.png')}); break;
+      case 18: this.setState({label: "Nerd Brain", imgSrc: require('./images/aspirations/18_nerd_brain.png')}); break;
+      case 19: this.setState({label: "Computer Whiz", imgSrc: require('./images/aspirations/19_computer_whiz.png')}); break;
       // Requires Vampires
-      case 20: this.setState({label: "Master Vampire", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 20: this.setState({label: "Master Vampire", imgSrc: require('./images/aspirations/20_master_vampire.png')}); break;
       // Requires Jungle Adventure
-      case 21: this.setState({label: "Archaeology Scholar", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 21: this.setState({label: "Archaeology Scholar", imgSrc: require('./images/aspirations/21_archaeology_scholar.png')}); break;
       // Requires Realm of Magic
-      case 22: this.setState({label: "Spellcraft & Sorcery", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 23: this.setState({label: "Serial Romantic", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 24: this.setState({label: "Soulmate", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 22: this.setState({label: "Spellcraft & Sorcery", imgSrc: require('./images/aspirations/22_spellcraft_&_sorcery.png')}); break;
+      case 23: this.setState({label: "Serial Romantic", imgSrc: require('./images/aspirations/23_serial_romantic.png')}); break;
+      case 24: this.setState({label: "Soulmate", imgSrc: require('./images/aspirations/24_soulmate.png')}); break;
       // Requires City Living
-      case 25: this.setState({label: "City Native", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 25: this.setState({label: "City Native", imgSrc: require('./images/aspirations/25_city_native.png')}); break;
       // Requires StrangerVille
-      case 26: this.setState({label: "StrangerVille Mystery", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 26: this.setState({label: "StrangerVille Mystery", imgSrc: require('./images/aspirations/26_strangerville_mystery.png')}); break;
       // Requires Island Living
-      case 27: this.setState({label: "Beach Life", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 28: this.setState({label: "Freelance Botanist", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 29: this.setState({label: "The Curator", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 30: this.setState({label: "Angling Ace", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 27: this.setState({label: "Beach Life", imgSrc: require('./images/aspirations/27_beach_life.png')}); break;
+      case 28: this.setState({label: "Freelance Botanist", imgSrc: require('./images/aspirations/28_freelance_botanist.png')}); break;
+      case 29: this.setState({label: "The Curator", imgSrc: require('./images/aspirations/29_the_curator.png')}); break;
+      case 30: this.setState({label: "Angling Ace", imgSrc: require('./images/aspirations/30_angling_ace.png')}); break;
       // Requires Outdoor Retreat
-      case 31: this.setState({label: "Outdoor Enthusiast", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 31: this.setState({label: "Outdoor Enthusiast", imgSrc: require('./images/aspirations/31_outdoor_enthusiast.png')}); break;
       // Requires Jungle Adventure
-      case 32: this.setState({label: "Jungle Explorer", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 32: this.setState({label: "Jungle Explorer", imgSrc: require('./images/aspirations/32_jungle_explorer.png')}); break;
       // Requires Realm of Magic
-      case 33: this.setState({label: "Purveyor of Potions", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 34: this.setState({label: "Joke Star", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 35: this.setState({label: "Party Animal", imgSrc: require('./images/aspirations/plumbob.png')}); break;
-      case 36: this.setState({label: "Friend of the World", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 33: this.setState({label: "Purveyor of Potions", imgSrc: require('./images/aspirations/33_purveyor_of_potions.png')}); break;
+      case 34: this.setState({label: "Joke Star", imgSrc: require('./images/aspirations/34_joke_star.png')}); break;
+      case 35: this.setState({label: "Party Animal", imgSrc: require('./images/aspirations/35_party_animal.png')}); break;
+      case 36: this.setState({label: "Friend of the World", imgSrc: require('./images/aspirations/36_friend_of_the_world.png')}); break;
       // Requires Get Together
-      case 37: this.setState({label: "Leader of the Pack", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 37: this.setState({label: "Leader of the Pack", imgSrc: require('./images/aspirations/37_leader_of_the_pack.png')}); break;
       // Requires Vampires
-      case 38: this.setState({label: "Good Vampire", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 38: this.setState({label: "Good Vampire", imgSrc: require('./images/aspirations/38_good_vampire.png')}); break;
       // Requires Get Famous
-      case 39: this.setState({label: "World Famous Celebrity", imgSrc: require('./images/aspirations/plumbob.png')}); break;
+      case 39: this.setState({label: "World-Famous Celebrity", imgSrc: require('./images/aspirations/39_world-famous_celebrity.png')}); break;
       default: break;
     }
 
